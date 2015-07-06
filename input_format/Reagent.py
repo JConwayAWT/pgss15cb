a7650db8e8c9f2d0ad8ee24f1c8674d238e5a02d
Konrad Urban <knurban@gmail.com>
2015-07-06 15:38:05 -0400

    Added Reagent class

---
+++ input_format/Reagent.py
@@ -0,0 +1,17 @@
+"""
+This file gives a class that defines an arbitrary reagent
+"""
+
+class Reagent(super):
+
+    def __init__(count, full_name, abb_name):
+        """Initializes Reagent class
+
+        count:     number of Reagent components
+        full_name: full name of reagent
+        abb_name:  abbreviated reagent name
+        """
+
+        self.count = count
+        self.full_name = full_name
+        self.abb_name = abb_name
