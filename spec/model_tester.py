4e77ae8535d4c0cf9b589878da2431a87a529210
Sohpie Fisher <sophiefisher1997@gmail.com>
2015-07-16 15:59:23 -0400

    added testing class to model_tester

--- spec/model_tester.py
+++ spec/model_tester.py
@@ -1,5 +1,21 @@
 #model_tester.py
-#updated version of testing to work with model.py
-
+#updated version of model tests
 class Result():
-    
+    def __init__(self, result = False, name = None, description = None, status = None):
+        self.result = result
+        self.name = name
+        self.description = description
+        self.status = status
+
+    def run_test( self, test_function ):
+        self.result = test_function()
+        if self.result.result == True:
+            print self.result.name
+            print "PASS / "
+        else:
+            print self.result.name
+            print self.result.description
+            print self.result.statustesting to work with model.py
+
+r = Result()
+r.run_test( neg_output )
