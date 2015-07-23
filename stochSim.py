bcc294aeadbe47fc3b2f9aaef71fe0aee7c5ed86
ksalotto <katelynsalotto98@gmail.com>
2015-07-23 16:57:33 -0400

    Used file arguments instead of filename arguments

--- stochSim.py
+++ stochSim.py
@@ -7,5 +7,5 @@ if len(sys.argv)!=3:
 	raise ValueError("This program needs 2 arguments: input file (.react) and output file (.csv)")
 
 p = Parser()
-model = p.get_model(sys.argv[1], sys.argv[2])
+model = p.get_model(open(sys.argv[1], "r"), open(sys.argv[2], "w"))
 model.iterate()
