68d0caa10b4274f0654a299e302950f97078f689
EpicVS <sharmav39@gmail.com>
2015-07-27 14:33:53 -0400

    fixed stochSim.py

--- stochSim.py
+++ stochSim.py
@@ -3,26 +3,27 @@
 
 from Parser import Parser
 import sys, argparse, string
-# if len(sys.argv)!=3:
-# 	raise ValueError("This program needs 2 arguments: input file (.react) and output file (.csv)")
-
-parser = argparse.ArgumentParser()
-
-parser.add_argument("infile", help="The .react file of the simulation that you want to run")
-parser.add_argument("-o","--outfile", help="The .csv file that you want to write the results of", default="")
-parser.add_argument("-t","--timeout", help="Max running time in seconds", type=int, default=-1)
-parser.add_argument("-l","--loggingfreq", help="Logging Frequency (default 100)", type=int, default=100)
 
 
-args = parser.parse_args()
+def run_stoch_sim(inFileName, outFileName, timeout=-1, loggingfreq=100):
+	p = Parser()
+	model = p.get_model(open(inFileName, 'r'), open(outFileName,'w+'), timeout, loggingfreq)
+	model.iterate()
 
-if args.outfile == "":
-	outfile = string.replace(args.infile, "react", "csv") if "react" in args.infile else args.infile+".csv"
-	print outfile
-else:
-	outfile = args.outfile
 
-p = Parser()
-print args.infile, args.outfile
-model = p.get_model(open(args.infile, 'r'), open(outfile,'w+'), args.timeout, args.loggingfreq)
-model.iterate()
+if __name__ == "__main__":
+	parser = argparse.ArgumentParser()
+
+	parser.add_argument("infile", help="The .react file of the simulation that you want to run")
+	parser.add_argument("-o","--outfile", help="The .csv file that you want to write the results of", default="")
+	parser.add_argument("-t","--timeout", help="Max running time in seconds", type=int, default=-1)
+	parser.add_argument("-l","--loggingfreq", help="Logging Frequency (default 100)", type=int, default=100)
+
+	args = parser.parse_args()
+
+	if args.outfile == "":
+		outfile = string.replace(args.infile, "react", "csv") if "react" in args.infile else args.infile+".csv"
+	else:
+		outfile = args.outfile
+
+	run_stoch_sim(args.infile, outfile, args.timeout, args.loggingfreq)
