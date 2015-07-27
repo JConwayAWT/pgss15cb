689736fd725fce5051aaa7e63aa0fb4141d11289
Jeff C <jconway@arcwebtech.com>
2015-07-27 11:39:50 -0400

    Provide the same variables in both places, heh, heh

--- skeletonpages/views.py
+++ skeletonpages/views.py
@@ -70,14 +70,7 @@ def show_simulation(request, simulation_id):
   for sublist in list_of_lists:
     dictionary.update( { sublist[0]: sublist[1:] } )
 
-  values_dictionary_as_json = json.dumps(dictionary)
-
-  # import ipdb
-  # ipdb.set_trace()
-
   context = {'simulation': simulation, 'simulation_values': mark_safe(dictionary)}
-
-
 
   return render_to_response('skeletonpages/show_simulation.html', context, RequestContext(request))
 
@@ -99,14 +92,28 @@ def create_simulation(request):
       output_file = out_file, name=request.POST['name'],
       description = request.POST['description'])
 
-    # import pdb
-    # pdb.set_trace()
-
     new_algorithm_run.save()
     request.user.userprofile.algorithm_runs.add(new_algorithm_run)
     request.user.userprofile.save()
 
-    context = {'simulation': new_algorithm_run}
+    output_file = open(new_algorithm_run.output_file.path, "r")
+    first_line = output_file.readline()
+
+    list_of_lists = []
+    for variable_name in first_line.strip().split(","):
+      list_of_lists.append([variable_name])
+
+    for line in output_file.readlines():
+      # ipdb.set_trace()
+      split_line = line.split(",")
+      for index, value in enumerate(split_line):
+        list_of_lists[index].append(float(value.rstrip()))
+
+    dictionary = {}
+    for sublist in list_of_lists:
+      dictionary.update( { sublist[0]: sublist[1:] } )
+
+    context = {'simulation': new_algorithm_run, 'simulation_values': mark_safe(dictionary)}
 
 
     return render_to_response('skeletonpages/show_simulation.html',
