29f6a5d27120dc454a86971115e07d3b9247ee49
EpicVS <sharmav39@gmail.com>
2015-07-27 10:37:39 -0400

    cmd args

--- stochSim.py
+++ stochSim.py
@@ -2,10 +2,25 @@
 """Main function"""
 
 from Parser import Parser
-import sys
-if len(sys.argv)!=3:
-	raise ValueError("This program needs 2 arguments: input file (.react) and output file (.csv)")
+import sys, argparse, string
+# if len(sys.argv)!=3:
+# 	raise ValueError("This program needs 2 arguments: input file (.react) and output file (.csv)")
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("infile", help="The .react file of the simulation that you want to run")
+parser.add_argument("-o","--outfile", help="The .csv file that you want to write the results of", default="")
+parser.add_argument("-t","--timeout", help="Max running time in seconds", type=int, default=-1)
+parser.add_argument("-l","--loggingfreq", help="Logging Frequency (default 1000)", type=int, default=1000)
+
+
+args = parser.parse_args()
+
+if args.outfile == "":
+	outfile = string.replace(args.outfile, "react", "csv") if "react" in args.infile else args.infile+".csv"
+else:
+	outfile = args.outfile
 
 p = Parser()
-model = p.get_model(sys.argv[1], sys.argv[2])
+model = p.get_model(args.infile, outfile)
 model.iterate()
