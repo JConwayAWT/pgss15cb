0c61f5add9f48e84401783ab79f2572a0ccbed11
Jeff C <jconway@arcwebtech.com>
2015-07-30 08:59:55 -0400

    Better hello

--- pgss15compbio/views.py
+++ pgss15compbio/views.py
@@ -7,4 +7,10 @@ from skeletonpages.models import *
 
 def hello(request):
   context = {'active_tab': '#index'}
-  return render_to_response('common/welcome.html', context, RequestContext(request))
+  if request.user.is_authenticated():
+    username = request.user.username
+    profile = request.user.userprofile
+    context = {'profile': profile, 'user': request.user}
+    return render_to_response('userena/profile_detail.html', context, RequestContext(request))
+  else:
+    return render_to_response('common/welcome.html', context, RequestContext(request))
